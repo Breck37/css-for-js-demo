8b19f3cecfcf18c27036b1decf30d231d29faf7b
Brent Eckert <brent.eckert7@gmail.com>
2021-09-20 21:50:36 -0700

    style: r1 refactored to use OuterStyledModule
    Signed-off-by: Brent Eckert <brent.eckert7@gmail.com>

--- pages/modules/rendering-one/index.js
+++ pages/modules/rendering-one/index.js
@@ -3,6 +3,7 @@ import Page from "../../../components/Page";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import R1 from "../../../components/r1";
+import { OuterModuleStyled } from "../../../components/styled/OuterModuleStyled";
 
 const RenderingOne = () => {
   const router = useRouter();
@@ -15,70 +16,12 @@ const RenderingOne = () => {
 
   return (
     <Page>
-      <ROneStyled>
+      <OuterModuleStyled>
         <h1>Rendering Logic One</h1>
         <R1 handlePick={handlePick} />
-      </ROneStyled>
+      </OuterModuleStyled>
     </Page>
   );
 };
 
 export default RenderingOne;
-
-const ROneStyled = styled.div`
-  height: 100%;
-  overflow: hidden;
-  overflow-y: scroll;
-  padding: 0 2rem;
-
-  ::-webkit-scrollbar {
-    display: none;
-  }
-
-  > div {
-    > ul {
-      height: calc(100% - 6rem);
-      overflow-y: scroll;
-      scrollbar-width: none;
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      grid-template-rows: repeat(3, 1fr);
-      grid-gap: 2rem;
-      padding: 1rem 1rem;
-
-      ::-webkit-scrollbar {
-        display: none;
-      }
-
-      > li {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        border: 1px solid rgba(0, 0, 0, 0.2);
-        -webkit-box-shadow: 0px 10px 13px -7px #000000,
-          5px 5px 15px 5px rgba(0, 0, 0, 0);
-        box-shadow: 0px 10px 13px -7px #000000,
-          5px 5px 15px 5px rgba(0, 0, 0, 0);
-      }
-    }
-  }
-
-  @media (max-width: 500px) {
-    padding: 2rem;
-    > div {
-      > ul {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-
-        > li {
-          width: 100%;
-          height: 4rem;
-        }
-      }
-    }
-  }
-`;
