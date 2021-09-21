96dcb9f7475d05150689d174343f6e7c1282ad58
Brent Eckert <brent.eckert7@gmail.com>
2021-09-20 21:48:28 -0700

    feat: r2/relative init
    Signed-off-by: Brent Eckert <brent.eckert7@gmail.com>

--- pages/modules/rendering-two.js
+++ pages/modules/rendering-two.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useMemo, useCallback, useState } from "react";
+import Page from "../../components/Page";
+import R2 from "../../components/r2";
+import { OuterModuleStyled } from "../../components/styled/OuterModuleStyled";
 
 const RenderingTwo = () => {
-  return <div>RenderingTwo</div>;
+  return (
+    <Page>
+      <OuterModuleStyled>
+        <h2>Rendering Logic Two</h2>
+        <R2 />
+      </OuterModuleStyled>
+    </Page>
+  );
 };
 
 export default RenderingTwo;
